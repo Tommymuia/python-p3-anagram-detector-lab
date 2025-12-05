486106a64fadc8f907c6260e01d60c0cb25aab31
Tommymuia <muiathomas.mt@gmail.com>
2025-12-05 11:23:25 +0300

    added

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+#!/usr/bin/env python3
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()  # store in lowercase for case-insensitive comparison
+
+    def match(self, word_list):
+        """Return a list of words from word_list that are anagrams of the initialized word."""
+        sorted_word = sorted(self.word)
+        return [w for w in word_list if sorted(w.lower()) == sorted_word]
